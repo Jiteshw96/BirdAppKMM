12c37a626bdf7df4d84290be2e97566140df3799
jitesh <jitesh.wadhwa13@gmail.com>
2024-02-20 14:32:28 +0530

    Compose Components

--- composeApp/src/commonMain/kotlin/tabs/Settings/SettingsScreen.kt
+++
@@ -1,2 +0,0 @@
-package tabs.Settings
-
--- composeApp/src/commonMain/kotlin/ui/components/BirdImageCell.kt
+++ composeApp/src/commonMain/kotlin/ui/components/BirdImageCell.kt
@@ -1,18 +1,25 @@
+import Ui.screens.BirdDetailScreen
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
 
 @Composable
 fun BirdImageCell(image :BirdImage){
+    val navigator : Navigator = LocalNavigator.currentOrThrow
     KamelImage(
         resource = asyncPainterResource(data= "https://sebastianaigner.github.io/demo-image-api/${image.path}"),
         contentDescription = "${image.category} by ${image.author}",
         contentScale = ContentScale.Crop,
-        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f)
-        
+        modifier = Modifier.fillMaxWidth().aspectRatio(1.0f).clickable {
+            navigator.push(BirdDetailScreen(image))
+        }
     )
 }
